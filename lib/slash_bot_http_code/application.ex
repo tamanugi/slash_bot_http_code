59a47b55240599729bcfd4927980993ce1155a1e
tamanugi <tamanugi@gmail.com>
2017-08-09 07:42:59 +0900

    modified application

--- lib/slash_bot_http_code/application.ex
+++ lib/slash_bot_http_code/application.ex
@@ -6,14 +6,12 @@ defmodule SlashBotHttpCode.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
+    SlashBotHttpCode.HttpCode.start_link
+
     children = [
-      # Starts a worker by calling: SlashBotHttpCode.Worker.start_link(arg)
-      # {SlashBotHttpCode.Worker, arg},
+      Plug.Adapters.Cowboy.child_spec(:http, SlashBotHttpCode.Router, [], [port: 4000])
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SlashBotHttpCode.Supervisor]
     Supervisor.start_link(children, opts)
   end
